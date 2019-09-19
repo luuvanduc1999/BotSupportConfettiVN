5f106dd839d971992224854882a3f7eed828cf67
Lưu Văn Đức <50072440+luuvanduc1999@users.noreply.github.com>
2019-09-19 21:56:48 +0700

    Delete cookie_pickle.py

--- cookie_pickle.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-import pickle
-
-driver = webdriver.Firefox()
-driver.get("https://www.google.com/search?q=hi")
-#khúc này import cookies vào rồi quay lại gõ bất kỳ
-s=input()
-pickle.dump( driver.get_cookies() , open("cookies.pkl","wb"))
