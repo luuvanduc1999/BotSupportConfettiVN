b12fb7eddb0cd6ce811f5b287aa2e9d62a9e0a81
Lưu Văn Đức <50072440+luuvanduc1999@users.noreply.github.com>
2019-09-19 21:57:06 +0700

    Update cft_telegram.py

--- cft_telegram.py
+++ cft_telegram.py
@@ -16,13 +16,13 @@ import pickle
 from colorama import Fore, Back, Style
 from colorama import init
 import time
+from halo import Halo
+
 
 init(autoreset=True)
-driver = webdriver.Firefox()
+driver = webdriver.Chrome()
 driver.get("http://www.google.com")
-cookies = pickle.load(open("cookies.pkl", "rb"))
-for cookie in cookies:
-    driver.add_cookie(cookie)
+
 
 def get_env(name, message, cast=str):
     if name in os.environ:
@@ -55,66 +55,9 @@ def split_string(source):
 				output[-1] = output[-1] + char
 	return output
 
-import os
-import json
-from bs4 import BeautifulSoup
-import argparse
-import sys
-import wx
-import sys
-import re
-import time
-from telethon import TelegramClient
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-import pickle
-import requests
-import pickle
-from colorama import Fore, Back, Style
-from colorama import init
-import time
+def SearchNEGG(qes,optionX):
 
-init(autoreset=True)
-driver = webdriver.Firefox()
-driver.get("http://www.google.com")
-cookies = pickle.load(open("cookies.pkl", "rb"))
-for cookie in cookies:
-    driver.add_cookie(cookie)
-
-def get_env(name, message, cast=str):
-    if name in os.environ:
-        return os.environ[name]
-    while True:
-        value = input(message)
-        try:
-            return cast(value)
-        except ValueError as e:
-            print(e, file=sys.stderr)
-            time.sleep(1)
-
-session = os.environ.get('TG_SESSION', 'printer')
-api_id = 892934
-api_hash = 'a1d95763f40384bfeee1c848f846888e'
-proxy = None  # https://github.com/Anorov/PySocks
-
-def split_string(source):
-	splitlist = ",!-.;/?@ #"
-	output = []
-	atsplit = True
-	for char in source:
-		if char in splitlist:
-			atsplit = True
-		else:
-			if atsplit:
-				output.append(char)
-				atsplit = False
-			else:
-				output[-1] = output[-1] + char
-	return output
     
-def SearchNEG(qes,optionX):
-
-    print("----------------------------------------")
 
     A1=0
     A2=0
@@ -156,12 +99,9 @@ def SearchNEG(qes,optionX):
     X1=G1
     X2=G2
     X3=G3
-    print(" ")
-    print(Fore.YELLOW+qes)
     f1=0
     f2=0
     f3=0
-    print()
     if ((X1<X2)&(X1<X3)):
         print(Back.CYAN+"[    Dap an: 11111   ]")
         f1=1
@@ -197,6 +137,7 @@ def SearchNEG(qes,optionX):
     print(Back.MAGENTA*f2+"2. "+optionX[1]+SPACE[1:LH-len(optionX[1])+1],"\t",G2,"\t",A2)
     print(Back.MAGENTA*f3+"3. "+optionX[2]+SPACE[1:LH-len(optionX[2])+1],"\t",G3,"\t",A3)
 
+'''y=document.getElementsByTagName("body")[0].innerText.toLowerCase(); X=y[0].match("GTK3");'''
 
 '''
 def SearchNEG(qes,option):
@@ -232,7 +173,6 @@ def SearchNEG(qes,option):
         print(Back.BLUE+"[    Dap an: 33333   ]\n")
 
     print(" ")
-'''
 
 def SearchWG(qes,optionX, HO):
 
@@ -312,86 +252,8 @@ def SearchWG(qes,optionX, HO):
     print(Back.MAGENTA*f1+"1. "+optionX[0]+SPACE[1:LH-len(optionX[0])+1],"\t",W1,"\t",G1+GD1/100)
     print(Back.MAGENTA*f2+"2. "+optionX[1]+SPACE[1:LH-len(optionX[1])+1],"\t",W2,"\t",G2+GD2/100)
     print(Back.MAGENTA*f3+"3. "+optionX[2]+SPACE[1:LH-len(optionX[2])+1],"\t",W3,"\t",G3+GD3/100)
-
-def Solve(str):
-    if ((str[0]=='#') & ((str[6]=='B') | (str[5]=='B'))):
-        os.system('cls')
-        start_time = time.time()
-        str=str.replace("Câu hỏi: ","")
-        X=str.splitlines()
-        HO=X[0]
-        qes=X[3]
-        neg=qes.count("KHÔNG")+qes.count("CHƯA")   
-        qes=qes.replace("KHÔNG","")
-        qes=qes.replace("CHƯA","")
-        O1=X[5][3:X[5].index('(')-1]
-        O2=X[6][3:X[6].index('(')-1]
-        O3=X[7][3:X[7].index('(')-1]
-        option=list()
-        option.append(O1)
-        option.append(O2)
-        option.append(O3)
-        if (neg>0):
-            print(Fore.YELLOW+qes)
-            ip=input("key =")
-            if (ip!=""):
-                qes="\""+ip+"\" "
-            SearchNEG(qes,option)
-        else:
-            SearchWG(qes,option,HO)
-        elapsed_time = time.time() - start_time
-        print("\nHoàn thành trong ",elapsed_time, " giây.\n")
-    else:
-        print(Fore.CYAN+str,"\n")
-    
-async def handler(update):
-    Solve(update.message.message)
-
-
-with TelegramClient(session, api_id, api_hash, proxy=proxy) as client:
-    client.add_event_handler(handler)
-    print('(Press Ctrl+C to stop this)')
-    client.run_until_disconnected()
 '''
-def SearchNEG(qes,option):
-
-
-    X1=0
-    X2=0
-    X3=0
-
-
-    driver.get("https://www.google.com/search?q="+qes+"\""+option[0]+"\"")
-    a=driver.find_element_by_id('resultStats')
-    x=a.text
-    X1=int(''.join(filter(str.isdigit, x)))
-    print("1. ", option[0],"\t", re.search('g (.+?) k',x).group(1))
-
-    driver.get("https://www.google.com/search?q="+qes+"\""+option[1]+"\"")
-    a=driver.find_element_by_id('resultStats')
-    x=a.text
-    X2=int(''.join(filter(str.isdigit, x)))
-    print("2. ", option[1],"\t", re.search('g (.+?) k',x).group(1))
-    driver.get("https://www.google.com/search?q="+qes+"\""+option[2]+"\"")
-    a=driver.find_element_by_id('resultStats')
-    x=a.text
-    X3=int(''.join(filter(str.isdigit, x)))
-    print("3. ", option[2],"\t", re.search('g (.+?) k',x).group(1))    
-    print(" ")
-    if ((X1<X2)&(X1<X3)):
-        print(Back.RED+"[    Dap an: 11111   ]\n")
-    elif ((X2<X1)&(X2<X3)):
-        print(Back.GREEN+"[    Dap an: 22222   ]\n")
-    elif ((X3<X1)&(X3<X2)):
-        print(Back.BLUE+"[    Dap an: 33333   ]\n")
-
-    print(" ")
-'''
-
-def SearchWG(qes,optionX, HO):
-
-    print("----------------------------------------")
-
+def SearchWG(qes,optionX):
     W1=0
     WD1=0
     W2=0
@@ -433,17 +295,26 @@ def SearchWG(qes,optionX, HO):
     for word in op3:
         GET="x=document.getElementById(\"search\"); y=x.textContent.toLowerCase(); z=y.match(/"+word.lower()+"/g); if (z==null) {return (0)} else {return z.length}"
         GD3+=driver.execute_script(GET)   
+    
+    driver.get("https://www.google.com/search?q="+qes.replace("\"","")+" ("+" OR ".join(optionX)+")")
+
+    GET="x=document.getElementsByClassName(\"rc\");y=x[0].textContent.toLowerCase();z=y.match(/"+optionX[0].lower()+"/g);KQ1=(z==null)?0:z.length;x=document.getElementById(\"search\"); return(KQ1);"
+    WD1=driver.execute_script(GET)
+    GET="x=document.getElementsByClassName(\"rc\");y=x[0].textContent.toLowerCase();z=y.match(/"+optionX[1].lower()+"/g);KQ1=(z==null)?0:z.length;x=document.getElementById(\"search\"); return(KQ1);"
+    WD2=driver.execute_script(GET)
+    GET="x=document.getElementsByClassName(\"rc\");y=x[0].textContent.toLowerCase();z=y.match(/"+optionX[2].lower()+"/g);KQ1=(z==null)?0:z.length;x=document.getElementById(\"search\"); return(KQ1);"
+    WD3=driver.execute_script(GET)
 
 
-    X1=G1+GD1/100+W1*10
-    X2=G2+GD2/100+W2*10
-    X3=G3+GD3/100+W3*10
-    print(" ")
-    print(Fore.YELLOW+qes)
+    X1=G1+GD1/1000+W1*10+WD1*2
+    X2=G2+GD2/1000+W2*10+WD2*2
+    X3=G3+GD3/1000+W3*10+WD3*2
+
     f1=0
     f2=0
     f3=0
     print()
+
     if ((X1>X2)&(X1>X3)):
         if (X1>=1):
             print(Back.CYAN+"[    Dap an: 11111   ]")
@@ -463,9 +334,10 @@ def SearchWG(qes,optionX, HO):
             print(Back.RED+"[ KHONG TIM DUOC DAP AN ]")
         f3=1
     print()
-    print(Back.MAGENTA*f1+"1. "+optionX[0]+SPACE[1:LH-len(optionX[0])+1],"\t",W1,"\t",G1+GD1/100)
-    print(Back.MAGENTA*f2+"2. "+optionX[1]+SPACE[1:LH-len(optionX[1])+1],"\t",W2,"\t",G2+GD2/100)
-    print(Back.MAGENTA*f3+"3. "+optionX[2]+SPACE[1:LH-len(optionX[2])+1],"\t",W3,"\t",G3+GD3/100)
+    print(Back.MAGENTA*f1+"1. "+optionX[0]+SPACE[1:LH-len(optionX[0])+1],"\t",W1+WD1/10,"\t",G1+GD1/1000)
+    print(Back.MAGENTA*f2+"2. "+optionX[1]+SPACE[1:LH-len(optionX[1])+1],"\t",W2+WD2/10,"\t",G2+GD2/1000)
+    print(Back.MAGENTA*f3+"3. "+optionX[2]+SPACE[1:LH-len(optionX[2])+1],"\t",W3+WD3/10,"\t",G3+GD3/1000)
+
 
 def Solve(str):
     if ((str[0]=='#') & ((str[6]=='B') | (str[5]=='B'))):
@@ -473,10 +345,12 @@ def Solve(str):
         start_time = time.time()
         str=str.replace("Câu hỏi: ","")
         X=str.splitlines()
-        HO=X[0]
+        print(X[0])
         qes=X[3]
         neg=qes.count("KHÔNG")+qes.count("CHƯA")   
         qes=qes.replace("KHÔNG","")
+        qes=qes.replace("Điền vào chỗ trống ","")
+        qes=qes.replace("Điền các chữ cái còn thiếu để ", " ")
         qes=qes.replace("CHƯA","")
         O1=X[5][3:X[5].index('(')-1]
         O2=X[6][3:X[6].index('(')-1]
@@ -485,14 +359,13 @@ def Solve(str):
         option.append(O1)
         option.append(O2)
         option.append(O3)
+        print("----------------------------------------")
+        print(Fore.YELLOW+qes)
+        print(" ")
         if (neg>0):
-            print(Fore.YELLOW+qes)
-            ip=input("key =")
-            if (ip!=""):
-                qes="\""+ip+"\" "
-            SearchNEG(qes,option)
+            SearchNEGG(qes,option)
         else:
-            SearchWG(qes,option,HO)
+            SearchWG(qes,option)
         elapsed_time = time.time() - start_time
         print("\nHoàn thành trong ",elapsed_time, " giây.\n")
     else:
